32e5404c838fdecf607aaee9dca293c2d172fb5d
mvinyard <mvinyard.ai@gmail.com>
2025-02-28 18:46:59 +0000

    update version to v0.1

--- ABCParse/__init__.py
+++ ABCParse/__init__.py
@@ -3,10 +3,13 @@ from ._function_kwargs import function_kwargs
 from ._as_list import as_list
 from . import logging
 
+from .__version__ import __version__
+
 
 __all__ = [
     "ABCParse",
     "function_kwargs",
     "as_list",
     "logging",
+    "__version__",
 ]
